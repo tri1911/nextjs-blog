6640bb16b3d77872f48cc92fdaea8a778748c5fe
Gary Yeung <gary.yeung>
2022-02-18 14:29:17 -0800

    chore: comment out `/` and `/posts/[id]` due to errors in build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
-import { GetStaticProps } from 'next';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
 
+/*
 const Home = ({
   allPostsData,
 }: {
@@ -49,3 +50,8 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
+*/
+
+export default function Home() {
+  return null;
+}
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,10 +1,11 @@
-import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import Head from 'next/head';
-import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
-import { GetStaticProps, GetStaticPaths } from 'next';
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
 
+/*
 const Post = ({ postData }) => {
   return (
     <Layout>
@@ -40,3 +41,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     },
   };
 };
+*/
+
+export default function Post() {
+  return null;
+}
