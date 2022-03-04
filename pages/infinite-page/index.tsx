f2d1841dcf6c00ca3068477f59c813a5472e34bf
Tri Ho <minhtri191193@gmail.com>
2022-03-03 17:14:58 -0800

    feat: use useEffect to subscribe to IntersectionObserver on the first render & log message when the component on/off screen

--- pages/infinite-page/index.tsx
+++ pages/infinite-page/index.tsx
@@ -1,11 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const DetectedBox = ({ id }: { id: string }) => {
+  const boxRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
   useEffect(() => {
     console.log(`Box ${id} is rendered...`);
+    // initialize observer
+    const observer = new IntersectionObserver(
+      (entries: any) => {
+        setIsVisible(entries[0].isIntersecting);
+      },
+      { threshold: [0] }
+    );
+    // subscribe
+    console.log(`box ${id} is subscribing...`);
+    boxRef.current && observer.observe(boxRef.current);
+    return () => {
+      // unsubscribe
+      console.log(`box ${id} is unsubscribing...`);
+      boxRef.current && observer.unobserve(boxRef.current);
+    };
   }, []);
+
+  useEffect(() => {
+    console.log(
+      isVisible ? `box ${id} is on screen` : `box ${id} is off screen`
+    );
+  }, [isVisible]);
+
   return (
-    <div className="bg-lime-500 h-80 w-80 my-5 mx-auto py-36 text-white uppercase font-bold text-5xl text-center">
+    <div
+      ref={boxRef}
+      className="bg-lime-500 h-80 w-80 my-96 mx-auto py-36 text-white uppercase font-bold text-5xl text-center"
+    >
       Box {id}
     </div>
   );
@@ -16,7 +44,6 @@ const InfinitePage = () => {
     <div>
       <DetectedBox id="1" />
       <DetectedBox id="2" />
-      <DetectedBox id="3" />
     </div>
   );
 };
